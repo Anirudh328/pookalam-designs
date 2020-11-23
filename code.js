c776094384a546e14ed2cbd4edbba127fa566d5f
Anirudh328 <74718128+Anirudh328@users.noreply.github.com>
2020-11-23 18:08:36 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,127 @@
+setProperty("start", "image", "assets/kathakali.jpg");
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("nect", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("hme", "click", function( ) {
+  setScreen("start");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("N", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("start");
+});
+onEvent("image1", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("image3", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("image4", "click", function( ) {
+  setScreen("screen8");
+});
+onEvent("image5", "click", function( ) {
+  setScreen("screen9");
+});
+onEvent("image2", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("image4", "click", function( ) {
+  setScreen("screen11");
+});
+onEvent("image6", "click", function( ) {
+  setScreen("screen12");
+});
+onEvent("image7", "click", function( ) {
+  setScreen("screen13");
+});
+onEvent("image9", "click", function( ) {
+  setScreen("screen14");
+});
+onEvent("image11", "click", function( ) {
+  setScreen("screen15");
+});
+onEvent("image8", "click", function( ) {
+  setScreen("screen16");
+});
+onEvent("image10", "click", function( ) {
+  setScreen("screen17");
+});
+onEvent("image12", "click", function( ) {
+  setScreen("screen18");
+});
+onEvent("image13", "click", function( ) {
+  setScreen("screen19");
+});
+onEvent("image14", "click", function( ) {
+  setScreen("screen20");
+});
+onEvent("image15", "click", function( ) {
+  setScreen("screen21");
+});
+onEvent("image16", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("image17", "click", function( ) {
+  setScreen("screen21");
+});
+onEvent("image18", "click", function( ) {
+  setScreen("screen23");
+});
+onEvent("image19", "click", function( ) {
+  setScreen("screen24");
+});
+onEvent("image20", "click", function( ) {
+  setScreen("screen25");
+});
+onEvent("image21", "click", function( ) {
+  setScreen("screen26");
+});
+onEvent("image22", "click", function( ) {
+  setScreen("screen27");
+});
+onEvent("image24", "click", function( ) {
+  setScreen("screen28");
+});
+onEvent("image25", "click", function( ) {
+  setScreen("screen29");
+});
+onEvent("back", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back3", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back4", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back5", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back6", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back7", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back8", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back9", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("back10", "click", function( ) {
+  setScreen("screen2");
+});
